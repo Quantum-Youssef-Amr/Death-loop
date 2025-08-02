8db1ce15b50f33c74e39a13af8cb106d926b15fe
Youssef amr <amry14003@gmail.com>
2025-08-02 15:54:32 +0200

    a small timer fix

--- Assets/scripts/UI/InGametimer.cs
+++ Assets/scripts/UI/InGametimer.cs
@@ -6,8 +6,8 @@ public class InGametimer : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timerMin;
     void Update()
     {
-        timerMin.text = $"{Mathf.RoundToInt(PatientSystem.Stats.TimeTillBrainDamage / 60f)}";
-        timerSec.text = $"{Mathf.RoundToInt(PatientSystem.Stats.TimeTillBrainDamage % 60f)}";
+        timerMin.text = $"0{Mathf.FloorToInt(PatientSystem.Stats.TimeTillBrainDamage / 60f)}";
+        timerSec.text = (Mathf.FloorToInt(PatientSystem.Stats.TimeTillBrainDamage % 60f) < 10 ? "0":"") + $"{Mathf.RoundToInt(PatientSystem.Stats.TimeTillBrainDamage % 60f)}";
     }
 
 
